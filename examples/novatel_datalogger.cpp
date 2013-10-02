aad1f34d6999c58b3f6ac5cbf5702b7357c2131c
Nathaniel Carson <ncarson891@gmail.com>
2013-10-02 13:07:04 -0500

    added code for gps ephemeris and compressed range to novatel_datalogger.cpp

--- examples/novatel_datalogger.cpp
+++ examples/novatel_datalogger.cpp
@@ -6,12 +6,32 @@ using namespace novatel;
 using namespace std;
 
 std::fstream file_;
+//************************************************creat two more of these for gpesephemB and rangecmpb
+void BestGpsPositionCallback(Position best_position, double time_stamp) {
 
-void BestPositionCallback(Position best_position, double time_stamp) {
+
+
+
 
 
 }
 
+void GpsEphemerisCallback(GpsEphemeris ephimeris, double time_stamp) {
+
+
+
+
+
+}
+
+void CompressedRangeMeasurementsCallback(CompressedRangeMeasurements compressed_range, double time_stamp)  {
+
+
+
+
+
+
+}
 
 
 bool OpenFile(std::string name) {
@@ -40,7 +60,17 @@ int main(int argc, char **argv)
     Novatel my_gps;
 
     my_gps.set_best_gps_position_callback(
-                boost::bind(&BestPositionCallback, this, _1, _2));
+                boost::bind(&BestGpsPositionCallback, this, _1, _2));
+
+
+    my_gps.set_gps_ephemeris_callback(
+                boost::bind(&GpsEphemerisCallback, this, _1, _2));
+
+
+     my_gps.set_compressed_range_measurements_callback(
+                boost::bind(&CompressedRangeMeasurementsCallback, this, _1, _2));
+
+
 
 
     bool result = my_gps.Connect(port,baudrate);
